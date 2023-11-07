5cfbbe6f44820cc7f2e7b50e464b9c2645625620
Sunday Ochigbo <131619007+sunday004@users.noreply.github.com>
2023-11-07 00:07:24 -0600

    Update ReadMate.jsx

--- src/pages/ReadMate.jsx
+++ src/pages/ReadMate.jsx
@@ -14,7 +14,7 @@ const ReadMates = ({data}) => {
         <div className="ReadMates">
             {
                 mates && mates.length > 0 ?
-                mates.map((mate,index) => 
+                [...mates].reverse().map((mate,index) => 
                    <Card id={mate.id} Name={mate.Name} IQ={mate.IQ}  Strength={mate.Strength} gender={mate.gender} key={index}/>
                 ) : <h2>{'No Crewmates Yet 😞'}</h2>
             }
